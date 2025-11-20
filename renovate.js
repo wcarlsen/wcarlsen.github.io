d3ccf08452ff3cb47ff577d9b258c5c859bc291f
Willi Carlsen <carlsenwilli@gmail.com>
2025-11-20 15:32:36 +0100

    Changed renovate from json to js
    Signed-off-by: Willi Carlsen <carlsenwilli@gmail.com>

---
+++ renovate.js
@@ -0,0 +1,19 @@
+module.exports = {
+  branchPrefix: 'update/renovate/',
+  username: 'renovate',
+  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+  onboarding: false,
+  platform: 'github',
+  repositories: [
+    'wcarlsen/wcarlsen.github.io',
+  ],
+  'pre-commit': {
+    enabled: true,
+  },
+  nix: {
+    enabled: true
+  },
+  lockFileMaintenance: {
+    enabled: true
+  },
+};
