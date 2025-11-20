c193904e45c3211bb527171580c4b1e85a3b2161
Willi Carlsen <carlsenwilli@gmail.com>
2025-11-20 15:34:18 +0100

    Added optional-config
    Signed-off-by: Willi Carlsen <carlsenwilli@gmail.com>

--- renovate.js
+++ renovate.js
@@ -3,6 +3,7 @@ module.exports = {
   username: 'renovate',
   gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
   onboarding: false,
+  requireConfig: 'optional',
   platform: 'github',
   repositories: [
     'wcarlsen/wcarlsen.github.io',
